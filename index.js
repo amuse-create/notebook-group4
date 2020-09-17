b10d5c54f2a615d648f7aed8a99a1bd66658a430
Alejandra Ossa Jimenez <alejandra.ossa.jimenez@gmail.com>
2020-09-17 17:05:34 +0200

    added javascript file to master

---
+++ index.js
@@ -0,0 +1,44 @@
+/* Global variables go here */
+/*Goran variables*/
+
+
+/*Alejandra variables*/
+
+
+
+/*Andrian variables*/
+
+
+/*Global variables end here */
+
+/* Goran Works from  here */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Goran work ends here*/
+
+/*Alejandra works from here */
+
+/*Alejandra work ends here */
+
+
+/*Adrian works from here */
+
+
+/*Adrian work ends here*/
+
+
+
+
+
