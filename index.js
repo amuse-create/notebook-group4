26a241fcc9f09f71cd7dc22a4ddfe16996632d6b
Alejandra Ossa Jimenez <alejandra.ossa.jimenez@gmail.com>
2020-09-19 15:13:02 +0200

    working on Add function in JS

--- index.js
+++ index.js
@@ -4,21 +4,36 @@
 
 /*Alejandra variables*/
 
-let editButtonCardHeader = document.getElementsByClassName("card-header");
-let editButtonCardBody = document.getElementByClassName("card-body");
+const listItems = document.forms[0];
+const itemInput = listItems.children[0];
+let todoItems = []
+let ul = document.querySelector('ul');
+let li = document.querySelector('li');
 
 
 /*Adrian variables*/
-
-
 /*Global variables end here */
-
 /* Goran Works from  here */
 /* Goran work ends here*/
 
 /*Alejandra works from here */
 
+// submit data function
+addItem = (event) => {
+    event.preventDefault();
+    if (itemInput.value != '') {
+        addListItem(itemInput.value);
+        itemInput.value = '';
+    } 
+}
+//load every event in the page
+listItems.addEventListener('submit', addItem);
 
+// add tasks
+addListItem = (task) => {
+    li.innerHTML = `<input type="checkbox"> <label> item 1 </label>`; ul.appendChild(li);
+    document.querySelector('.to-do-list').style.display = 'block';
+}
 
 
 
@@ -29,8 +44,4 @@ let editButtonCardBody = document.getElementByClassName("card-body");
 
 
 /*Adrian work ends here*/
-
-
-
-
-
+            
