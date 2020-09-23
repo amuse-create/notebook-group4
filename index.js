3f2f01101c2d5ef079975d4dd8c9a07441fb6cb5
GoeranLE <goeran.lehmann@gmail.com>
2020-09-23 14:59:07 +0200

    checkbox update

--- index.js
+++ index.js
@@ -1,49 +1,50 @@
-/* Global variables go here */
-/*Goran variables*/
-
-
 /*Alejandra variables*/
-
 const listItems = document.forms[0];
-const itemInput = listItems.children[0];
-let todoItems = []
-let ul = document.querySelector('.taskList');
-let li = document.createElement('li');
-
+const itemInput = listItems.children[2];
+const todoItems = [];
+const ul = document.getElementById("list");
+const tasks = document.getElementsByClassName("tasks");
 /*Adrian variables*/
 /*Global variables end here */
 /* Goran Works from  here */
+
 /* Goran work ends here*/
 
 /*Alejandra works from here */
 
+// mark a task as done
 
-// submit data function
-let addItem = (event) => {
-    event.preventDefault();
-    if (itemInput.value != '') {
-        addListItem(itemInput.value);
-itemInput.value = '';
-    } 
-    todoItems.push(itemInput);
-}
+const markTaskAsDone = (event) => {
+  const label = event.currentTarget.parentNode.children[1];
+  label.classList.toggle("finishedTask");
+};
 
 // add tasks
-let addListItem = (todoItems) => {
-    li.innerText = `${todoItems}` ; 
-    ul.appendChild(li);
-    document.querySelector('.newTask').style.display = 'block';
-   
-}
+const addListItem = (tasks) => {
+  const li = document.createElement("li");
+  li.classList.add("task");
+  const taskTemplate = `
+    <input type="checkbox" name="my-checkbox" id="opt-in">
+    <label for="opt-in">${tasks}</label> 
+  `;
+  li.innerHTML = taskTemplate;
+  ul.appendChild(li);
+  const checkboxes = document.querySelectorAll("input[type='checkbox']");
+  for (let checkbox of checkboxes) {
+    checkbox.addEventListener("click", markTaskAsDone);
+  }
+  document.querySelector(".text-container").style.display = "block";
+};
+
+// submit data function
+const addItem = (event) => {
+  event.preventDefault();
+  if (itemInput.value) {
+    addListItem(itemInput.value);
+    itemInput.value = "";
+    todoItems.push(addItem);
+  }
+};
 
 //load every event in the page
-listItems.addEventListener('submit', addItem);
-
-
-/*Alejandra work ends here */
-
-
-/*Adrian works from here */
-
-
-/*Adrian work ends here*/ 
+listItems.addEventListener("submit", addItem);
